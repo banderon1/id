a8e896c8c8b2c10cc736fe55cb48bce9e85678b1
Gavin Smiley <gavin.race16@gmail.com>
2017-01-07 12:40:25 -0800

    Added scripts folder and card box triggers

---
+++ Assets/GavinFolders/Scripts/CardBoxTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBoxTrigger : MonoBehaviour {
+
+public GameObject enteredCard;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "Card")
+		{
+			enteredCard = col.gameObject;
+
+			switch (enteredCard.GetComponent<CardDestination>().destination)
+
+			{
+				case 0: 
+					print("Go to first level");
+					break;
+				case 1:
+					print("Go to second level");
+					break;
+				case 2:
+					print("Go to third level");
+					break;
+				case 3: 
+					print("Go to fourth level");
+					break;
+				case 4:
+					print("Go to fifth level");
+					break;
+				default:
+					print("Something is wrong");
+					break;
+
+			}
+		}
+	}
+}
---
+++ Assets/GavinFolders/Scripts/CardDestination.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDestination : MonoBehaviour {
+
+	public int destination;
+
+}
